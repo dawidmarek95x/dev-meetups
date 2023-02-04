f3253c82335d39e7d932a1a89c2aa4cb5ab6e2bb
dawidmarek95x <dawid.marek95x@gmail.com>
2023-02-04 20:38:01 +0100

    Updated GET request for database-based detailed meeting pages

--- src/pages/[meetupId]/index.tsx
+++ src/pages/[meetupId]/index.tsx
@@ -1,9 +1,10 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { dbConnection, dbDisconnection } from "@/services/mongoDb";
+import { ObjectId } from "mongodb";
 import { GetStaticPaths, GetStaticProps } from "next";
-import meetupsData from "../../data/meetups.json";
 
 interface Meetup {
-  id: string;
+  _id: string;
   title: string;
   image: string;
   address: string;
@@ -26,13 +27,15 @@ const MeetupDatails = ({ meetup }: MeetupDetailsProps) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = await meetupsData.map((m) => {
-    return {
-      params: {
-        meetupId: m.id,
-      },
-    };
-  });
+  const meetupsCollection = await dbConnection("dev-meetups");
+  const meetupsData = await meetupsCollection.find().toArray();
+  dbDisconnection();
+
+  const paths = await meetupsData.map((meetup) => ({
+    params: {
+      meetupId: meetup._id.toString(),
+    },
+  }));
 
   return {
     paths: [...paths],
@@ -41,12 +44,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const meetupId = ctx.params?.meetupId ?? "";
-  const meetup = await meetupsData.find((m) => m.id === meetupId);
+  let meetupId = ctx.params?.meetupId ?? "";
+  if (Array.isArray(meetupId)) {
+    meetupId = meetupId.join("");
+  }
+
+  const meetupsCollection = await dbConnection("dev-meetups");
+  const query = { _id: new ObjectId(meetupId) };
+  const meetupData = await meetupsCollection.findOne(query);
+  dbDisconnection();
 
   return {
     props: {
-      meetup,
+      meetup: {
+        ...meetupData,
+        _id: meetupId,
+      },
     },
   };
 };
